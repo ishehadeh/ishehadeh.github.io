0a9e8dad8258f25968c00c8f1e9592f0a3dd777f
Ian Shehadeh <ian@shehadeh.net>
2026-03-02 19:48:53 -0500

    set up blog

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -5,4 +5,13 @@ export default function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("www/fonts");
 	eleventyConfig.addPassthroughCopy("www/papers");
 	eleventyConfig.addPassthroughCopy("www/assets");
+
+	eleventyConfig.addFilter("readableDate", (dateObj) => {
+		return new Intl.DateTimeFormat('en-US', {
+		  year: 'numeric',
+		  month: 'long',
+		  day: 'numeric',
+		  timeZone: 'UTC' // Prevents "off-by-one" day errors
+		}).format(dateObj);
+	  });
 }
